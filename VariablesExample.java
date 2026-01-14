f8fcf632db6990446e2bafc60cda12be6380cc2d
dogakaramese <doa@karamese.net>
2026-01-14 13:31:35 +0100

    Create VariablesExample.java

---
+++ VariablesExample.java
@@ -0,0 +1,12 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+
+        int age = 21;
+        double height = 1.65;
+        String name = "Doga";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+}
