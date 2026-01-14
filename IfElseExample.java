12c854e541e6a2c36b06d81da2aafe90dad04b0f
dogakaramese <doa@karamese.net>
2026-01-14 13:38:40 +0100

    Create IfElseExample.java

---
+++ IfElseExample.java
@@ -0,0 +1,14 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+
+        int number = 8;
+
+        if (number > 0) {
+            System.out.println("Number is positive");
+        } else if (number < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+    }
+}
