00ad445a34913be49950daec4b70f0a5029c3ad4
Doga <doa@karamese.net>
2026-01-14 13:48:30 +0100

    Create ArraysExample.java

---
+++ ArraysExample.java
@@ -0,0 +1,11 @@
+public class ArraysExample {
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 5, 7, 9, 11};
+
+        // print array elements using a loop
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
